fb70b5deb2516204920e24deb0d4f7a0a0be21a8
Olivia_Shamon <olish585@student.liu.se>
2021-04-19 17:00:50 +0200

    feat: get data from user in db

--- xperience/frontend/src/components/eventComponent.js
+++ xperience/frontend/src/components/eventComponent.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 
 export default class CreateEvent extends Component {
+
+
+
   render() {
     return (
       <div>
         <p>React Create Event Component!</p>
       </div>
+
+      
     );
   }
 }
