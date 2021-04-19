f98707b922134943c98b5e63d4d530c18f0deffd
Olivia_Shamon <olish585@student.liu.se>
2021-04-19 02:25:20 +0200

    feat: add basic routing to application, change from js to jsx for app, use material-ui

---
+++ xperience/frontend/src/components/eventComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class CreateEvent extends Component {
+  render() {
+    return (
+      <div>
+        <p>React Create Event Component!</p>
+      </div>
+    );
+  }
+}
--- xperience/frontend/src/index.js
+++ xperience/frontend/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import 'fontsource-roboto';
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+ </BrowserRouter>,
   document.getElementById('root')
 );
 
