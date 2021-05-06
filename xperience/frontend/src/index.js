758f9b792956d771498efbd22831323748f6103c
Olivia_Shamon <olish585@student.liu.se>
2021-05-06 11:44:09 +0200

    chore: remove dependcies

--- xperience/frontend/src/index.js
+++ xperience/frontend/src/index.js
@@ -9,7 +9,6 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { configureStore } from './app/Store/configureStore';
-import { loadEvents } from './features/events/eventActions';
 
 const store = configureStore();
 
