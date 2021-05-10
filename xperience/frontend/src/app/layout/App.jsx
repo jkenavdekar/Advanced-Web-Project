3e7cabbc013c80ecd3d4281f8faefb475b45127c
Olivia Shamon <olish585@student.liu.se>
2021-05-10 10:48:14 +0200

    chore: remove 404 page

--- xperience/frontend/src/app/layout/App.jsx
+++ xperience/frontend/src/app/layout/App.jsx
@@ -13,7 +13,6 @@ import ModalManager from '../common/modals/ModalManager';
 import { useDispatch } from 'react-redux';
 import { loadEvents } from '../../features/events/eventActions';
 import ProfilePage from '../../features/profile/ProfilePage';
-import PageNotFound from '../../features/pageNotFound/PageNotFound';
 
 function App() {
 
@@ -43,7 +42,6 @@ function App() {
             <Route path={['/createEvent', '/manage/:id']} component={EventForm} key={key} />
             <Route path='/account' component={AccountPage} />
             <Route path='/profile/:id' component={ProfilePage} />
-            <Route path="*" component={PageNotFound} />
         </Container>
         
         </>
