48f6399b58b45749e459883be6eb0d5d36eb70c6
Olivia Shamon <olish585@student.liu.se>
2021-05-19 14:50:10 +0200

    chore: remove extra files

--- xperience/xperience/frontend/src/features/profile/profileActions.js
+++
@@ -1,8 +0,0 @@
-import { LISTEN_TO_CURRENT_USER_PROFILE } from './profleConstants';
-
-export function listenToCurrentUserProfile(profile) {
-    return {
-        type: LISTEN_TO_CURRENT_USER_PROFILE,
-        payload: profile
-    }   
-}
